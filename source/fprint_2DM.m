bf60e0043ce1654b299d1d3ad0144279dc76073c
Andy Ridgwell <andy@seao2.org>
2024-11-05 11:42:40 -0800

    -

--- source/fprint_2DM.m
+++ source/fprint_2DM.m
@@ -11,12 +11,12 @@ format0 = PFORMAT0;
 opt_flip = PFLIP;
 opt_nonan = PNAN;
 %
-if isempty(data_mask),
+if isempty(data_mask)
     data_mask = zeros(jmax,imax);
     data_mask = data_mask + 1.0;
 end
 %
-if opt_flip,
+if opt_flip
     data_save = flipud(data_save); 
     data_mask = flipud(data_mask); 
 end
@@ -29,7 +29,7 @@ end
 fid = fopen(filename,'w');
 for j = jmax:-1:1
     for i = 1:imax
-        if (data_mask(j,i) == 1),
+        if (data_mask(j,i) == 1)
             fprintf(fid,format,data_save(j,i));
         else
             fprintf(fid,format0,0);
